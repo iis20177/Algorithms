9fb51410ab9758f3b163d0934a145e3528876145
jovannafrag <ioanpan11@gmail.com>
2024-08-23 01:12:13 +0300

    c programming exercise solutions

--- 2η εργασία/SocialFriends/SocialFriends.c
+++ 2η εργασία/SocialFriends/SocialFriends.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Function(int array[13][13], int start[]);
-
 int main(){
-    int i, j, sum[13], k, count = 0, max, pos;
+    int i, j, sum[13], k, count = 0, max, pos, temp;
     int Friends[13][13] =  {{0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
                             {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                             {1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
@@ -32,63 +30,77 @@ int main(){
                             "Petros",
                             "Dhmhtris",
                             "Anna"  };
-    Function(Friends, &sum);
-    for (k=0; k<13; k++)
-    {
-        max = sum[0];
-        for (j=1; j<13; j++)
-            {
-                if (max < sum[j])
-                    {
-                        max = sum[j];
-                        pos = j;
-                    }
-            }
-        for (i=0; i<13; i++)
-            {
-                for (j=0; j<13; j++)
-                    {
-                        if (Friends[pos][j] == 1)
-                            {
-                                Friends[pos][j] = 0;
-                                Friends[j][pos] = 0;
-                            }
-                    }
-            }
-    }
-    Function(Friends, &sum);
-    for (j=0; j<13; j++)
-        {
-            if (sum[j] < 2)
-                {
-                    count++;
-                }
-        }
-    printf("Plithos twn atomwn pou tha pane sto party: %d", count);
     for (i=0; i<13; i++)
         {
-            if (sum[i] < 2)
-            {
-                printf("\n%s", Names[i]);
-            }
+            sum[i] = 0;
         }
-    return 0;
-}
-
-void Function(int array[13][13], int start[]){
-    int a, b;
-    for (a=0; a<13; a++)
+    for (i=0; i<13; i++)
         {
-            start[a] = 0;
-        }
-    for (a=0; a<13; a++)
-        {
-            for (b=0; b<13; b++)
+            for (j=0; j<13; j++)
                 {
-                    if (array[a][b] == 1)
+                    if (Friends[i][j] == 1)
                         {
-                            start[a]++;
+                            sum[i]++;
+                        }
+
+                }
+        }
+     for (k=0; k<13; k++)
+        {
+            for (i=0; i<13; ++i)
+                {
+                    for (j=i+1; j<13; ++j)
+                    {
+                        if (sum[i] < sum[j] && sum[i] > 2)
+                        {
+                            temp = sum[i];
+                            sum[i] = sum[j];
+                            sum[j] = temp;
+                        }
+                    }
+                }
+            for (i=0; i<13; i++)
+                {
+                    sum[i] = pos;
+                    for (j=0; j<13; j++)
+                        {
+                            if (Friends[pos][j] == 1)
+                                {
+                                    Friends[pos][j] = -1;
+                                    Friends[j][pos] = -1;
+                                }
+                        }
+                }
+            for (i=0; i<13; i++)
+                {
+                    sum[i] = 0;
+                }
+            for (i=0; i<13; i++)
+                {
+                    for (j=0; j<13; j++)
+                        {
+                            if (Friends[i][j] == 1)
+                                {
+                                    sum[i]++;
+                                }
+
                         }
                 }
         }
+        for (j=0; j<13; j++)
+            {
+                if (sum[j] == 0)
+                    {
+                        count++;
+                    }
+            }
+        printf("Plithos twn atomwn pou tha pane sto party: %d", count);
+        for (i=0; i<13; i++)
+            {
+                if (sum[i] == 0)
+                    {
+                        printf("\n%s", Names[i]);
+                    }
+            }
+    return 0;
 }
